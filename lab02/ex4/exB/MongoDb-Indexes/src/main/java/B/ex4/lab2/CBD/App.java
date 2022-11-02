c64bf007cbca6c74d4e42ae3a9e771fb11bcef4f
DiogoAlves002 <diogoasilva@ua.pt>
2022-11-02 14:59:44 +0000

    initial commit for lab2/ex4/B

---
+++ lab02/ex4/exB/MongoDb-Indexes/src/main/java/B/ex4/lab2/CBD/App.java
@@ -0,0 +1,13 @@
+package B.ex4.lab2.CBD;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ lab02/ex4/exB/MongoDb-Indexes/src/test/java/B/ex4/lab2/CBD/AppTest.java
@@ -0,0 +1,20 @@
+package B.ex4.lab2.CBD;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
