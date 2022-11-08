c9a9e1e4ff7ae641a13b89ea7b5d07524e5cb5dc
DiogoAlves002 <diogoasilva@ua.pt>
2022-11-08 00:30:24 +0000

    lab2 ex4 B alomost done

--- lab02/ex4/exB/MongoDb-Indexes/src/main/java/B/ex4/lab2/CBD/App.java
+++ lab02/ex4/exB/MongoDb-Indexes/src/main/java/B/ex4/lab2/CBD/App.java
@@ -1,13 +1,40 @@
 package B.ex4.lab2.CBD;
 
-/**
- * Hello world!
- *
- */
+import com.mongodb.client.*;
+import com.mongodb.client.model.Indexes;
+import com.mongodb.client.model.Updates;
+
+import java.util.Arrays;
+
+import org.bson.Document;
+import org.bson.conversions.Bson;
+ 
 public class App 
 {
+    static void listALl(MongoCollection<Document> collection){
+        FindIterable<Document> insertedDocs = collection.find();
+        for (Object doc : insertedDocs) { 
+            System.out.println(doc);
+        }
+    }
+
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        MongoClient mongoClient = MongoClients.create();
+        MongoDatabase database = mongoClient.getDatabase("cbd");
+        MongoCollection<Document> collection = database.getCollection("restaurants");
+
+
+        listALl(collection);
+
+        collection.createIndex(Indexes.ascending("localidade"));
+        collection.createIndex(Indexes.ascending("gastronomia"));
+        collection.createIndex(Indexes.text("nome"));
+
+        FindIterable<Document> insertedDocs = collection.find(("localidade", "aveiro"));
+        for (Document doc : insertedDocs) {
+            System.out.println(doc.toJson());
+        }
+
     }
 }
